40f1e5fb178e4e16a4b062ed8224555483912177
Kaynã Camargo <kayna.khad@gmail.com>
2022-10-09 14:10:28 -0300

    get all repos on github account

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let response = reqwest::blocking::get("https://github.com/Vowye?tab=repositories")
+        .unwrap()
+        .text()
+        .unwrap();
+
+    let document = scraper::Html::parse_document(&response);
+
+    let title_selector = scraper::Selector::parse("li>div>div>h3>a").unwrap();
+
+    let titles = document.select(&title_selector).map(|x| x.inner_html());
+
+    titles
+        .zip(1..101)
+        .for_each(|(item, number)| println!("{}. {}", number, item));
 }
